d74428690c495f5268ecc289bbff6d175483df08
Hamza Amhal <hamza.amhal@yousign.com>
2021-12-02 23:17:57 +0100

    HA: Calculate Earning Function with tests.

--- app.ts
+++ app.ts
@@ -1,8 +1,34 @@
+/**
+ * Daily roll coaster earning
+ * @param seatsCount Available seat in the roll coaster
+ * @param ridesCount Rides for the day
+ * @param groups Groups of people waiting in the queue
+ * @returns Final day recipie
+ */
 export const calculateEarning = (
   seatsCount: number,
-  ridesNumber: number,
+  ridesCount: number,
   groups: number[]
 ) => {
+  // Final day recipie
   let earning: number = 0;
+  for (let i = 0; i < ridesCount; i++) {
+    // Ride total amount.
+    let totalRideAmount = 0;
+    // All groups that has been passed.
+    const pastGroups = [];
+    while (totalRideAmount + groups[0] <= seatsCount) {
+      // Current group that want to take the roll coaster
+      const currentGroup: number = groups.shift() as number;
+      // Pushing the value to the pastGroups
+      pastGroups.push(currentGroup);
+      // Adding the amount
+      totalRideAmount += currentGroup;
+    }
+    // Adding earning to the global earning
+    earning += totalRideAmount;
+    // Pushing new values so all the ones who did the roll coaster will be in the last of the list
+    groups.push(...pastGroups);
+  }
   return earning;
 };
--- calculate-earning.test.ts
+++ calculate-earning.test.ts
@@ -2,12 +2,20 @@ import { expect, describe } from "@jest/globals";
 import { calculateEarning } from "./app";
 
 describe("Earning calculation", () => {
-  test("it should be equal to 0", () => {
+  test("it should equal to 0", () => {
     const output = calculateEarning(0, 0, []);
     expect(output).toEqual(0);
   });
-  test("it should be equal to 1", () => {
+  test("it should equal to 1", () => {
     const output = calculateEarning(1, 1, [1]);
     expect(output).toEqual(1);
+  });
+  test("it should equal to 7", () => {
+    const output = calculateEarning(3, 3, [3, 1, 1, 2]);
+    expect(output).toEqual(7);
+  });
+  test("it should equal to 100", () => {
+    const output = calculateEarning(10, 100, [1]);
+    expect(output).toEqual(100);
   });
 });
